68098ce6429da8ecfa2be1985cc5eacbaec09d91
Muhammad Talha <mt109208@gmail.com>
2026-06-16 11:30:02 +0500

    url corrected

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chatApplication/', include('chatapp.urls')),
+    path('', include('chatapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
